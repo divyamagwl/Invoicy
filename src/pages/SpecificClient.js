68ed5070da8bc214b9db15a63ad232a8c592a348
GSri30 <sri_3007@hotmail.com>
2022-01-13 20:53:24 +0530

    minor fixes

--- src/pages/SpecificClient.js
+++ src/pages/SpecificClient.js
@@ -170,21 +170,21 @@ class ClientDashboard extends React.Component {
                             <h6 className="text-muted">{web3.utils.fromWei(invoice.data.payment.dueAmount)} ETH</h6>
                         </td>
                         <td>
-                            <h6>Work Completed: &nbsp; 
+                            <h6 className="text-muted">Status: &nbsp; 
                             {
                                 invoice.data.workCompleted &&
-                                "Yes"
+                                <span className="text-success">Completed</span>
                             }
                             {
                                 !invoice.data.workCompleted &&
-                                "No"
+                                <span className="text-danger">Not Completed</span>
                             }
                             </h6>
-                            <button style={{border: 0}} onClick={() => this.updateWorkStatus(invoice.id)} className="label theme-bg text-white f-12">Reverse</button>
                         </td>
                         <td>
                             <a href={DEMO.BLANK_LINK} className="label theme-bg2 text-white f-12">View Details</a>
-                            <button style={{border: 0}} onClick={() => this.dialog.showAlert('Reminder sent!')} className="label theme-bg text-white f-12">Remind</button>
+                            <button style={{border: 0}} onClick={() => this.updateWorkStatus(invoice.id)} className="label theme-bg text-white f-12">Update Progress</button>
+                            <button style={{border: 0}} onClick={() => this.dialog.showAlert('Reminder sent!')} className="label theme-bg2 text-white f-12">Remind</button>
                             <Dialog ref={(component) => { this.dialog = component }} />
                         </td>
                     </tr>
