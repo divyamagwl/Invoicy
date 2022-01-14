7ddef7138712921bcc3878147ba5adc933bcf93c
GaganAgarwal77 <Gagan.Agarwal@iiitb.ac.in>
2022-01-15 01:58:20 +0530

    Updates

---
+++ precache-manifest.2097448bcde9766827964af71c607c99.js
@@ -0,0 +1,162 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee3f04277fc570417b7149ca6dc88c38",
+    "url": "/InvoiceManagementSystem/index.html"
+  },
+  {
+    "revision": "c0db4efbdcfccd2d49a6",
+    "url": "/InvoiceManagementSystem/static/css/1.da14042e.chunk.css"
+  },
+  {
+    "revision": "f153204f465e43d41007",
+    "url": "/InvoiceManagementSystem/static/css/12.b3e34d5b.chunk.css"
+  },
+  {
+    "revision": "34ab137a24b4f5d02f5d",
+    "url": "/InvoiceManagementSystem/static/css/2.6f2fc3bd.chunk.css"
+  },
+  {
+    "revision": "f6aa566fd2a5e6cb5893",
+    "url": "/InvoiceManagementSystem/static/css/4.9f2578c5.chunk.css"
+  },
+  {
+    "revision": "7f2d824e86fad3acdcd2",
+    "url": "/InvoiceManagementSystem/static/css/5.594b8ad5.chunk.css"
+  },
+  {
+    "revision": "e6e002f0eedf6640288a",
+    "url": "/InvoiceManagementSystem/static/css/8.1df05c62.chunk.css"
+  },
+  {
+    "revision": "168e1471ba8a4df79f9d",
+    "url": "/InvoiceManagementSystem/static/css/9.d9cb926e.chunk.css"
+  },
+  {
+    "revision": "2f706457df87f2c47f08",
+    "url": "/InvoiceManagementSystem/static/js/0.ad378eb5.chunk.js"
+  },
+  {
+    "revision": "c0db4efbdcfccd2d49a6",
+    "url": "/InvoiceManagementSystem/static/js/1.a784bdef.chunk.js"
+  },
+  {
+    "revision": "95f372bac81cd22731d0117f8db09368",
+    "url": "/InvoiceManagementSystem/static/js/1.a784bdef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8159e472a7931e4642ee",
+    "url": "/InvoiceManagementSystem/static/js/10.5f5c9ba1.chunk.js"
+  },
+  {
+    "revision": "729f26fab73485d56d110b9f5f026b87",
+    "url": "/InvoiceManagementSystem/static/js/10.5f5c9ba1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aade31eaa7c66b5ca7e1",
+    "url": "/InvoiceManagementSystem/static/js/11.60997c34.chunk.js"
+  },
+  {
+    "revision": "f153204f465e43d41007",
+    "url": "/InvoiceManagementSystem/static/js/12.2c1b9041.chunk.js"
+  },
+  {
+    "revision": "e9afe8bb03c18aa2cbce",
+    "url": "/InvoiceManagementSystem/static/js/13.bed7cf13.chunk.js"
+  },
+  {
+    "revision": "47731724e4582ed14e5c",
+    "url": "/InvoiceManagementSystem/static/js/14.f4bfb432.chunk.js"
+  },
+  {
+    "revision": "e557517519f0fed8bd60",
+    "url": "/InvoiceManagementSystem/static/js/15.537188b5.chunk.js"
+  },
+  {
+    "revision": "dc4ec0969c490681d669",
+    "url": "/InvoiceManagementSystem/static/js/16.5dfba0d4.chunk.js"
+  },
+  {
+    "revision": "1943c0843756d16c0a73",
+    "url": "/InvoiceManagementSystem/static/js/17.41303aaa.chunk.js"
+  },
+  {
+    "revision": "4960afd09695e53901bc",
+    "url": "/InvoiceManagementSystem/static/js/18.0bc8aa22.chunk.js"
+  },
+  {
+    "revision": "34ab137a24b4f5d02f5d",
+    "url": "/InvoiceManagementSystem/static/js/2.7006cbf9.chunk.js"
+  },
+  {
+    "revision": "af7867681200cea4869fe338ec3790ea",
+    "url": "/InvoiceManagementSystem/static/js/2.7006cbf9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d3da75fe609d9665622",
+    "url": "/InvoiceManagementSystem/static/js/3.96902c55.chunk.js"
+  },
+  {
+    "revision": "fe07165234709e61e0cdc05d4056de5c",
+    "url": "/InvoiceManagementSystem/static/js/3.96902c55.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6aa566fd2a5e6cb5893",
+    "url": "/InvoiceManagementSystem/static/js/4.3b534183.chunk.js"
+  },
+  {
+    "revision": "7f2d824e86fad3acdcd2",
+    "url": "/InvoiceManagementSystem/static/js/5.c6bffcee.chunk.js"
+  },
+  {
+    "revision": "e6e002f0eedf6640288a",
+    "url": "/InvoiceManagementSystem/static/js/8.79321860.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/InvoiceManagementSystem/static/js/8.79321860.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "168e1471ba8a4df79f9d",
+    "url": "/InvoiceManagementSystem/static/js/9.8cafaaa4.chunk.js"
+  },
+  {
+    "revision": "1a122f47ae8cf3e0df47",
+    "url": "/InvoiceManagementSystem/static/js/main.fad278cb.chunk.js"
+  },
+  {
+    "revision": "b5a64c9a0731b3487e16",
+    "url": "/InvoiceManagementSystem/static/js/runtime-main.c6e1d238.js"
+  },
+  {
+    "revision": "5af81432dda1dde2def604f8052f424a",
+    "url": "/InvoiceManagementSystem/static/media/download.5af81432.svg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/InvoiceManagementSystem/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/InvoiceManagementSystem/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/InvoiceManagementSystem/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/InvoiceManagementSystem/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/InvoiceManagementSystem/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "15c01d9b81ceb6702aa92e3b5416ce50",
+    "url": "/InvoiceManagementSystem/static/media/remove.15c01d9b.svg"
+  },
+  {
+    "revision": "d4da359f65106dae13fb51495f035419",
+    "url": "/InvoiceManagementSystem/static/media/resize.d4da359f.svg"
+  }
+]);
